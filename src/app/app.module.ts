1f74c60f904fd5dbba86de6c368b60402b698a3b
Aldin <aldin.bijedic@gmx.at>
2022-05-22 15:16:46 +0200

    Alert component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionFormComponent } from './question-form/question-form.component';
+import { AlertComponent } from './shared/alert/alert.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuestionFormComponent
+    QuestionFormComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
