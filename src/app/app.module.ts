2d1e256ea00443607ebe167184037fee9d42e0bc
Aldin <aldin.bijedic@gmx.at>
2022-05-23 17:54:44 +0200

    Added AddChildComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuestionFormComponent } from './question-form/question-form.component';
 import { AlertComponent } from './shared/alert/alert.component';
+import { AddChildComponent } from './add-child/add-child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestionFormComponent,
-    AlertComponent
+    AlertComponent,
+    AddChildComponent
   ],
   imports: [
     BrowserModule,
