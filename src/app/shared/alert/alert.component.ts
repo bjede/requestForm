1ec1e93eb9795b555458b56a2761652d6bb5a422
Aldin <aldin.bijedic@gmx.at>
2022-05-26 11:16:43 +0200

    Added @Input() decorator

--- src/app/shared/alert/alert.component.ts
+++ src/app/shared/alert/alert.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-alert',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./alert.component.scss']
 })
 export class AlertComponent implements OnInit {
+
+  @Input() alertMessage = '';
+  @Input() alertColor = '';
 
   constructor() { }
 
