3c002031579dc7f63aa2d5a657cd3910f1d79303
Aldin <aldin.bijedic@gmx.at>
2022-05-30 09:40:48 +0200

    Created interface IRequestData

---
+++ src/app/shared/interfaces/irequest-data.ts
@@ -0,0 +1,19 @@
+export interface IRequestData {
+    startDate: Date,
+    endDate: Date,
+    adults: number,
+    childName: string,
+    birthday: number,
+    monthOfBirth: string,
+    yearOfBirth: number,
+    firstName: string,
+    lastName: string,
+    email: string,
+    street: string,
+    phoneNumber: number,
+    zipCode: number,
+    city: string,
+    country: string,
+    message: string,
+    children: any[]
+  }
