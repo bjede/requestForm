8dce0ba91297a1029a03c7005bb2a81a5fa9780e
Aldin <aldin.bijedic@gmx.at>
2022-05-30 09:45:24 +0200

    Validation class for request form created and logic moved.

---
+++ src/app/models/request-form-validation.ts
@@ -0,0 +1,155 @@
+import { AbstractControl, FormArray, FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+
+export class RequestFormValidation {
+
+    fb = new FormBuilder();
+
+    // Create required to be input field startDate
+    startDate = new FormControl('', [
+        Validators.required,
+    ]);
+
+    // Create required to be input field endDate
+    endDate = new FormControl('', [
+        Validators.required,
+    ]);
+
+    // Create required to be input field amountOfAdults
+    amountOfAdults = new FormControl('', [
+        Validators.required
+    ]);
+
+    // Create required to be input field childName
+    childName = new FormControl('', [
+        Validators.required
+    ]);
+
+    // Create required to be input field birthday
+    birthday = new FormControl(null, [
+        Validators.required
+    ]);
+
+    // Create required to be inputcfield monthOfBirth
+    monthOfBirth = new FormControl(null, [
+        Validators.required
+    ]);
+
+    // Create required to be input field yearOfBirth
+    yearOfBirth = new FormControl(null, [
+        Validators.required
+    ]);
+
+    // Create required to be input field firstName
+    firstName = new FormControl('', [
+        Validators.required
+    ]);
+
+
+    // Create required to be input field lastName
+    lastName = new FormControl('', [
+        Validators.required
+    ]);
+
+    // Create required to be input field email
+    email = new FormControl('', [
+        Validators.required,
+        Validators.email
+    ]);
+
+    // Create required to be input field street
+    street = new FormControl('', [
+        Validators.required
+    ]);
+
+    // Create required to be input field phoneNumber
+    phoneNumber = new FormControl('', [
+        Validators.pattern('[^a-zA-Z\*?:<>.]+')
+    ]);
+
+    // Create required to be input field zipCode
+    zipCode = new FormControl('', [
+        Validators.required
+    ]);
+
+    // Create required to be input field city
+    city = new FormControl('', [
+        Validators.required
+    ]);
+
+    // Create required to be input field country
+    country = new FormControl(null, [
+        Validators.required
+    ]);
+
+    // Create input to be field message without required field
+    message = new FormControl('');
+
+    /**
+     *  Create FormGroup instance.
+     *  Tracks the value and validity state of a group of FormControl instances.
+     */
+    requestForm = new FormGroup({
+        startDate: this.startDate,
+        endDate: this.endDate,
+        adults: this.amountOfAdults,
+        childName: this.childName,
+        birthday: this.birthday,
+        monthOfBirth: this.monthOfBirth,
+        yearOfBirth: this.yearOfBirth,
+        firstName: this.firstName,
+        lastName: this.lastName,
+        email: this.email,
+        street: this.street,
+        phoneNumber: this.phoneNumber,
+        zipCode: this.zipCode,
+        city: this.city,
+        country: this.country,
+        message: this.message,
+        children: this.fb.array([])
+    });
+
+    /**
+     * 
+     * Create new child input fields incl. validation. Name, Birthday, Month of Birthday, Year of Birth.
+     * focus property is important for the focus event.
+     * 
+     * @returns - FormGroup 
+     */
+    createNewChildInputFields(): FormGroup {
+        return this.fb.group({
+            childName: ['', Validators.required],
+            childBirthday: [null, Validators.required],
+            childMonth: [null, Validators.required],
+            childYear: [null, Validators.required],
+            focus: false,
+        });
+    }
+
+
+    /**
+     *  Returns the "children" array.
+     */
+    get childControl(): AbstractControl[] {
+        return (<FormArray>this.requestForm.get('children')).controls;
+    }
+
+
+    /**
+     *  Add new fields for the child and push them into the array
+     */
+    addNewChilrensdFields() {
+        const control = (<FormArray>this.requestForm.get('children'));
+        control.push(this.createNewChildInputFields());
+    }
+
+
+    /**
+     * Remove the correct input fields from the children array.
+     *  
+     * @param i - number The index to be removed from the children array.
+     */
+    removeChildInputSection(i: number) {
+        (<FormArray>this.requestForm.get('children')).removeAt(i);
+    }
+
+}
